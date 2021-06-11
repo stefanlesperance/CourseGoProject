7d88e51026fe32d9ada89176664a55990a0ec401
Stefan L'Esperance <slesperance@conamex.com>
2021-06-11 11:21:04 -0400

    Trix Working - Put require-trix before boostrap fixed the issue.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+require("trix")
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
 require("stylesheets/application.scss")
+require("@rails/actiontext")
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require("trix")
+require("@rails/actiontext")
--- app/models/course.rb
+++ app/models/course.rb
@@ -4,4 +4,6 @@ class Course < ApplicationRecord
 	def to_s
 		title
 	end
+	has_rich_text :description
+
 end
