4ffe5f7299615bd3fdb9ac1523c98098e31300c2
Stefan L'Esperance <slesperance@conamex.com>
2021-06-16 11:32:16 -0400

    Fixed Devise Issue and No User Front Page issue.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :set_user, only: [:edit, :update]
+	before_action :set_user, only: [:show, :edit, :update]
 	def index
 		#@users = User.all.order(created_at: :desc)
 
--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -4,9 +4,37 @@ class CoursePolicy < ApplicationPolicy
       scope
     end
   end
+  
+  def show?
+    @user.present? && @user.has_role?(:admin) ||
+    @user.present? && @record.user_id == @user.id ||
+    @user.present? && @record.bought(@user)
+  end
+
+
 
   def edit?
-    @user.has_role?:admin
+    @record.user == @user
+  end
+
+  def update?
+    @record.user == @user
+  end
+
+  def new?
+    @user.has_role?(:teacher)
+  end
+
+
+
+  def create?
+    @user.has_role?(:teacher)
+  end
+
+
+
+  def destroy?
+   @record.user == @user
   end
 
 end
