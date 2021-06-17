368c2c0c6474fe430867f1ad9e9885820747ee10
Stefan L'Esperance <slesperance@conamex.com>
2021-06-17 10:04:15 -0400

    Lessons, Breadcrumb Lessons, Various Edits.

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,18 @@ class Lesson < ApplicationRecord
   belongs_to :course
   validates :title, :content, :course, presence: true
 
+  has_rich_text :content
+
   extend FriendlyId
   friendly_id :title, use: :slugged
   
+    include PublicActivity::Model
+    # We track the owner by assigning it based on the current user
+    tracked owner: Proc.new{ |controller, model| controller.current_user}
+
+    #This converts the title to a proper string from the ActiveRecord piece it currently is.
+    def to_s
+      title
+    end
+
 end
