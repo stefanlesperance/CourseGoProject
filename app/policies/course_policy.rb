2424e450a074062ed4275b3e9472c5a617f85b29
Stefan L'Esperance <slesperance@conamex.com>
2021-06-18 05:49:33 -0400

    Policies for Lessons.

--- app/policies/course_policy.rb
+++ app/policies/course_policy.rb
@@ -12,29 +12,24 @@ class CoursePolicy < ApplicationPolicy
   end
 
 
-
   def edit?
-    @record.user == @user
+    @user.has_role?(:admin) || @record.user_id == user.id
   end
 
   def update?
-    @record.user == @user
+    @user.has_role?(:admin) || @record.user_id == user.id
   end
 
   def new?
     @user.has_role?(:teacher)
   end
 
-
-
   def create?
     @user.has_role?(:teacher)
   end
 
-
-
   def destroy?
-   @record.user == @user
+   @user.has_role?(:admin) || @record.user_id == user.id
   end
 
 end
