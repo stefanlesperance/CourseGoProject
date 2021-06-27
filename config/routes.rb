23573c033e8841e5b1615aaa69b93842c73d5d43
Stefan L'Esperance <slesperance@conamex.com>
2021-06-27 06:05:47 -0400

    Test.

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,7 @@ Rails.application.routes.draw do
   get 'activity', to: 'home#activity'
   get 'analytics', to: 'home#analytics'
 
+  #For test purposes.
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
